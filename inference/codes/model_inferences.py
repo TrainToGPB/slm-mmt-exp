fb0ed51c3293796329afff3d0259a0a27267f88f
Sehyeong Kim <sehyeong_kim@tmax.co.kr>
2024-01-11 06:41:45 +0000

    refactor: generalize model inference and evaluation functions

--- inference/codes/model_inferences.py
+++ inference/codes/model_inferences.py
@@ -6,47 +6,88 @@ import sys
 import torch
 import pandas as pd
 from transformers import MarianMTModel, MarianTokenizer
+from transformers import MBartForConditionalGeneration, MBart50Tokenizer
+from transformers import M2M100ForConditionalGeneration, NllbTokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 # custom
 sys.path.append('../../../')
 from custom_utils.training_utils import set_seed
 
 
-def load_model_and_tokenizer(model_name):
-    model = MarianMTModel.from_pretrained(model_name)
-    tokenizer = MarianTokenizer.from_pretrained(model_name)
+def load_model_and_tokenizer(model_type):
+    assert model_type in ['opus', 'mbart', 'nllb', 'madlad'], 'Error: Wrong Model Type'
+
+    if model_type == 'opus':
+        model_name = 'Helsinki-NLP/opus-mt-tc-big-en-ko'
+        model = MarianMTModel.from_pretrained(model_name)
+        tokenizer = MarianTokenizer.from_pretrained(model_name)
+    elif model_type == 'mbart':
+        model_name = 'facebook/mbart-large-50-many-to-many-mmt'
+        model = MBartForConditionalGeneration.from_pretrained(model_name)
+        tokenizer = MBart50Tokenizer.from_pretrained(model_name)
+    elif model_type == 'nllb':
+        model_name = 'facebook/nllb-200-distilled-600M'
+        model = M2M100ForConditionalGeneration.from_pretrained(model_name)
+        tokenizer = NllbTokenizer.from_pretrained(model_name)
+    elif model_type == 'madlad':
+        model_name = 'google/madlad400-3b-mt'
+        model = T5ForConditionalGeneration.from_pretrained(model_name)
+        tokenizer = T5Tokenizer.from_pretrained(model_name)
+    
     return model, tokenizer
 
 
-def translate_text(model, tokenizer, text, device, max_length=512):
+def translate(model, tokenizer, text, model_type, device, max_length=512):
+    model.to(device)
+
+    if model_type == 'madlad':
+        text = '<2ko> ' + text
+
+    if model_type == 'mbart':
+        tokenizer.src_lang = 'en_XX'
+    elif model_type == 'nllb':
+        tokenizer.src_lang = 'eng_Latn'
+
     inputs = tokenizer(text, return_tensors="pt", max_length=max_length, truncation=True)
     inputs = {key: value.to(device) for key, value in inputs.items()}
-    model.to(device)
-    outputs = model.generate(**inputs)
+
+    if model_type == 'opus' or model_type == 'madlad':
+        outputs = model.generate(**inputs)
+    elif model_type == 'mbart':
+        outputs = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id['ko_KR'])
+    elif model_type == 'nllb':
+        outputs = model.generate(**inputs, forced_bos_token_id=tokenizer.lang_code_to_id['kor_Hang'])
+
     translated_text = tokenizer.batch_decode(outputs, skip_special_tokens=True)[0]
+
     return translated_text
 
 
-def inference(model_name, source_column, target_column, eval_path, device):
+def inference(model_type, source_column, target_column, eval_path, output_path, device):
     set_seed(42)
-
-    eval_df = pd.read_csv(eval_path)
-
-    model, tokenizer = load_model_and_tokenizer(model_name)
+    model, tokenizer = load_model_and_tokenizer(model_type)
     model.to(device)
 
-    eval_df[target_column] = eval_df[source_column].apply(lambda text: translate_text(model, tokenizer, text, device))
-
-    output_csv_path = "../results/test_tiny_uniform100_inferenced.csv"
-    eval_df.to_csv(output_csv_path, index=False)
+    eval_df = pd.read_csv(eval_path)
+    eval_df[target_column] = eval_df[source_column].apply(lambda text: translate(model, tokenizer, text, model_type, device))
+    eval_df.to_csv(output_path, index=False)
 
 
 if __name__ == '__main__':
-    MODEL_NAME = "Helsinki-NLP/opus-mt-tc-big-en-ko"
+    """
+    [MODEL_TYPE]
+    - opus
+    - mbart
+    - nllb
+    - madlad
+    """
+    MODEL_TYPE = "madlad"
     SOURCE_COLUMN = "en"
-    TARGET_COLUMN = "opus_trans"
+    TARGET_COLUMN = MODEL_TYPE + "_trans"
     EVAL_PATH = "../results/test_tiny_uniform100_inferenced.csv"
+    OUTPUT_PATH = "../results/test_tiny_uniform100_inferenced.csv"
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("DEVICE:", DEVICE)
 
-    inference(MODEL_NAME, SOURCE_COLUMN, TARGET_COLUMN, EVAL_PATH, DEVICE)
+    inference(MODEL_TYPE, SOURCE_COLUMN, TARGET_COLUMN, EVAL_PATH, OUTPUT_PATH, DEVICE)
--- inference/evaluation/inference_evaluation.py
+++ inference/evaluation/inference_evaluation.py
@@ -16,17 +16,11 @@ def calculate_sentence_bleu(reference, candidate):
     return bleu * 100
 
 
-def calculate_bleu(eval_path):
-    eval_df = pd.read_csv(eval_path)
-
+def calculate_bleu(eval_df, column_name):
     references = eval_df['ko'].apply(lambda x: [x]).tolist()
-    candidates_google = eval_df['google_trans'].tolist()
-    candidates_deepl = eval_df['deepl_trans'].tolist()
-
-    bleu_google = corpus_bleu(references, candidates_google)
-    bleu_deepl = corpus_bleu(references, candidates_deepl)
-
-    return bleu_google * 100, bleu_deepl * 100
+    candidates = eval_df[column_name].tolist()
+    bleu = corpus_bleu(references, candidates)
+    return bleu * 100
 
 
 def calculate_sentence_rouge(reference, candidate):
@@ -37,46 +31,74 @@ def calculate_sentence_rouge(reference, candidate):
     return rouge_1 * 100, rouge_2 * 100
 
 
-def calculate_rouge(eval_path):
-    eval_df = pd.read_csv(eval_path)
-    rouge_1_google, rouge_2_google = zip(*eval_df.apply(lambda row: calculate_sentence_rouge(row['ko'], row['google_trans']), axis=1))
-    rouge_1_deepl, rouge_2_deepl = zip(*eval_df.apply(lambda row: calculate_sentence_rouge(row['ko'], row['deepl_trans']), axis=1))
-    return (get_average(rouge_1_google), get_average(rouge_2_google)), (get_average(rouge_1_deepl), get_average(rouge_2_deepl))
+def calculate_rouge(eval_df, column_name):
+    rouge_1_scores, rouge_2_scores = zip(*eval_df.apply(lambda row: calculate_sentence_rouge(row['ko'], row[column_name]), axis=1))
+    rouge_1, rouge_2 = get_average(rouge_1_scores), get_average(rouge_2_scores)
+    return rouge_1, rouge_2
 
 
 def calculate_sentence_wer(reference, candidate):
     return Levenshtein.distance(reference.split(), candidate.split())
 
 
-def calculate_wer(eval_path):
-    eval_df = pd.read_csv(eval_path)
-    wer_google = eval_df.apply(lambda row: calculate_sentence_wer(row['ko'], row['google_trans']), axis=1)
-    wer_deepl = eval_df.apply(lambda row: calculate_sentence_wer(row['ko'], row['deepl_trans']), axis=1)
-    return get_average(wer_google), get_average(wer_deepl)
+def calculate_wer(eval_df, column_name):
+    wer_scores = eval_df.apply(lambda row: calculate_sentence_wer(row['ko'], row[column_name]), axis=1)
+    wer = get_average(wer_scores)
+    return wer
 
 
-def calculate_sacrebleu(eval_path, tokenizer_name='gogamza/kobart-base-v2'):
-    eval_df = pd.read_csv(eval_path)
+def calculate_sacrebleu(eval_df, column_name, tokenizer_name='gogamza/kobart-base-v2'):
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
     references = eval_df['ko'].tolist()
-    candidates_google = eval_df['google_trans'].tolist()
-    candidates_deepl = eval_df['deepl_trans'].tolist()
+    candidates = eval_df[column_name].tolist()
 
     references = [tokenizer.tokenize(ref) for ref in references]
-    candidates_google = [tokenizer.tokenize(can) for can in candidates_google]
-    candidates_deepl = [tokenizer.tokenize(can) for can in candidates_deepl]
+    candidates = [tokenizer.tokenize(can) for can in candidates]
 
     # sacrebleu = evaluate.load('sacrebleu')
-    sacrebleu_google = [sentence_bleu(references, candidate) for candidate in tqdm(candidates_google)]
-    sacrebleu_deepl = [sentence_bleu(references, candidate) for candidate in tqdm(candidates_deepl)]
+    sacrebleu_scores = [sentence_bleu(references, candidate) for candidate in tqdm(candidates)]
+    sacrebleu = get_average(sacrebleu_scores) * 100
 
-    return get_average(sacrebleu_google) * 100, get_average(sacrebleu_deepl) * 100
+    return sacrebleu
+
+
+def evaluate_all(eval_df, column_list, metric_list):
+    eval_dict = dict()
+    for col in column_list:
+        print(f"Evaluting Column {col}..")
+        col_dict = dict()
+        if 'bleu' in metric_list:
+            mean_bleu = calculate_bleu(eval_df, col)
+            col_dict['bleu'] = mean_bleu
+        if 'sacrebleu' in metric_list:
+            mean_sacrebleu = calculate_sacrebleu(eval_df, col)
+            col_dict['sacrebleu'] = mean_sacrebleu
+        if 'rouge' in metric_list:
+            mean_rouge_1, mean_rouge_2 = calculate_rouge(eval_df, col)
+            col_dict['rouge_1'] = mean_rouge_1
+            col_dict['rouge_2'] = mean_rouge_2
+        if 'wer' in metric_list:
+            mean_wer = calculate_wer(eval_df, col)
+            col_dict['wer'] = mean_wer
+
+        eval_dict[col] = col_dict
+
+    return eval_dict
+
+
+def print_evaluation_results(eval_dict):
+    for trans in eval_dict.keys():
+        print(f"[{trans.upper()}]")
+        for metric_key, metric_value in eval_dict[trans].items():
+            print(f" - {metric_key.upper()}: {metric_value:.2f}")
 
 
 if __name__ == '__main__':
     eval_path = '../results/test_tiny_uniform100_inferenced.csv'
+    eval_df = pd.read_csv(eval_path)
     
+    # evaluate sentence
     # u100_eval = pd.read_csv(eval_path)
     # sentence_num = 200
     # reference = u100_eval['ko'][sentence_num]
@@ -84,7 +106,17 @@ if __name__ == '__main__':
     # print(calculate_sentence_bleu(reference, candidate))
     # print(calculate_sentence_rouge(reference, candidate))
 
-    print(calculate_bleu(eval_path))
-    print(calculate_sacrebleu(eval_path))
-    print(calculate_rouge(eval_path))
-    print(calculate_wer(eval_path))
+    # evaluate dataset
+    """
+    [COLUMN_LIST]
+    papago_trans: 네이버 파파고 API (현재 개수 미달)
+    google_trans: 구글 번역 API
+    deepl_trans: 딥엘 API
+    opus_trans: Helsinki-NLP/opus-mt-tc-big-en-ko (HuggingFace)
+    mbart_trans: facebook/mbart-large-50-many-to-many-mmt (HuggingFace)
+    nllb_trans: facebook/nllb-200-distilled-600M (HuggingFace)
+    """
+    column_list = ['nllb_trans']
+    metric_list = ['bleu', 'sacrebleu', 'rouge', 'wer']
+    eval_dict = evaluate_all(eval_df, column_list, metric_list)
+    print_evaluation_results(eval_dict)
