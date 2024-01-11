46f9f41398c6fd8be5359329e9b78f5e040b1e0d
Sehyeong Kim <sehyeong_kim@tmax.co.kr>
2024-01-11 02:29:55 +0000

    feat: add translation datasets handling codes

---
+++ translation_datasets/aihubshell_translation.sh
@@ -0,0 +1,19 @@
+# 전문분야
+aihubshell -mode d -datasetkey 111
+# 기술과학
+aihubshell -mode d -datasetkey 124
+# 사회과학
+aihubshell -mode d -datasetkey 125
+# 병렬
+aihubshell -mode d -datasetkey 126
+# 산업정보(특허)
+aihubshell -mode d -datasetkey 563
+# 일상생활 및 구어체
+aihubshell -mode d -datasetkey 71265
+# 기술과학
+aihubshell -mode d -datasetkey 71266
+# 방송콘텐츠
+aihubshell -mode d -datasetkey 71382
+
+# 기계번역 품질검증
+aihubshell -mode d -datasetkey 71475
