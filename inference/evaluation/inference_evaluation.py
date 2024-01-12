ecf14d85cc478b2c742df3d7151ca147ce1e95c8
Sehyeong Kim <sehyeong_kim@tmax.co.kr>
2024-01-12 07:08:31 +0000

    feat: add some util functions and docstrings to all functions

--- inference/evaluation/inference_evaluation.py
+++ inference/evaluation/inference_evaluation.py
@@ -1,3 +1,5 @@
+import yaml
+
 import pandas as pd
 from nltk.translate.bleu_score import sentence_bleu, corpus_bleu
 from rouge import Rouge
@@ -8,15 +10,44 @@ from tqdm import tqdm
 
 
 def get_average(tuple_data):
+    """
+    Calculate the average of a tuple of numerical values.
+
+    Parameters:
+    - tuple_data (tuple): Tuple of numerical values.
+
+    Returns:
+    - average (float): Average value.
+    """
     return sum(tuple_data) / len(tuple_data)
 
 
 def calculate_sentence_bleu(reference, candidate):
+    """
+    Calculate the BLEU score for a pair of reference and candidate sentences.
+
+    Parameters:
+    - reference (str): Reference sentence.
+    - candidate (str): Candidate sentence.
+
+    Returns:
+    - bleu (float): BLEU score (percentage).
+    """
     bleu = sentence_bleu([list(reference.split())], list(candidate.split()))
     return bleu * 100
 
 
 def calculate_bleu(eval_df, column_name):
+    """
+    Calculate the BLEU score for a given column in the evaluation dataframe.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - column_name (str): Name of the column to be evaluated.
+
+    Returns:
+    - bleu (float): BLEU score (percentage).
+    """
     references = eval_df['ko'].apply(lambda x: [x]).tolist()
     candidates = eval_df[column_name].tolist()
     bleu = corpus_bleu(references, candidates)
@@ -24,6 +55,17 @@ def calculate_bleu(eval_df, column_name):
 
 
 def calculate_sentence_rouge(reference, candidate):
+    """
+    Calculate the ROUGE scores (ROUGE-1 and ROUGE-2) for a pair of reference and candidate sentences.
+
+    Parameters:
+    - reference (str): Reference sentence.
+    - candidate (str): Candidate sentence.
+
+    Returns:
+    - rouge_1 (float): ROUGE-1 score (percentage).
+    - rouge_2 (float): ROUGE-2 score (percentage).
+    """
     rouge = Rouge()
     rouge_scores = rouge.get_scores(candidate, reference)
     rouge_1 = rouge_scores[0]['rouge-1']['f']
@@ -32,22 +74,64 @@ def calculate_sentence_rouge(reference, candidate):
 
 
 def calculate_rouge(eval_df, column_name):
+    """
+    Calculate the average ROUGE scores (ROUGE-1 and ROUGE-2) for a given column in the evaluation dataframe.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - column_name (str): Name of the column to be evaluated.
+
+    Returns:
+    - rouge_1 (float): Average ROUGE-1 score (percentage).
+    - rouge_2 (float): Average ROUGE-2 score (percentage).
+    """
     rouge_1_scores, rouge_2_scores = zip(*eval_df.apply(lambda row: calculate_sentence_rouge(row['ko'], row[column_name]), axis=1))
     rouge_1, rouge_2 = get_average(rouge_1_scores), get_average(rouge_2_scores)
     return rouge_1, rouge_2
 
 
 def calculate_sentence_wer(reference, candidate):
+    """
+    Calculate the Word Error Rate (WER) for a pair of reference and candidate sentences.
+
+    Parameters:
+    - reference (str): Reference sentence.
+    - candidate (str): Candidate sentence.
+
+    Returns:
+    - wer (int): Word Error Rate.
+    """
     return Levenshtein.distance(reference.split(), candidate.split())
 
 
 def calculate_wer(eval_df, column_name):
+    """
+    Calculate the average Word Error Rate (WER) for a given column in the evaluation dataframe.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - column_name (str): Name of the column to be evaluated.
+
+    Returns:
+    - wer (float): Average Word Error Rate.
+    """
     wer_scores = eval_df.apply(lambda row: calculate_sentence_wer(row['ko'], row[column_name]), axis=1)
     wer = get_average(wer_scores)
     return wer
 
 
 def calculate_sacrebleu(eval_df, column_name, tokenizer_name='gogamza/kobart-base-v2'):
+    """
+    Calculate the sacrebleu score for a given column in the evaluation dataframe.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - column_name (str): Name of the column to be evaluated.
+    - tokenizer_name (str, optional): Name of the tokenizer. Default is 'gogamza/kobart-base-v2'.
+
+    Returns:
+    - sacrebleu (float): sacrebleu score (percentage).
+    """
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
 
     references = eval_df['ko'].tolist()
@@ -64,6 +148,17 @@ def calculate_sacrebleu(eval_df, column_name, tokenizer_name='gogamza/kobart-bas
 
 
 def evaluate_all(eval_df, column_list, metric_list):
+    """
+    Evaluate multiple metrics for each column in the evaluation dataframe.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - column_list (list): List of column names to be evaluated.
+    - metric_list (list): List of metrics to be calculated ('bleu', 'sacrebleu', 'rouge', 'wer').
+
+    Returns:
+    - eval_dict (dict): Dictionary containing evaluation results for each column and metric.
+    """
     eval_dict = dict()
     for col in column_list:
         print(f"Evaluting Column {col}..")
@@ -87,11 +182,53 @@ def evaluate_all(eval_df, column_list, metric_list):
     return eval_dict
 
 
+def evaluate_by_source(eval_df, source_list, column_list, metric_list):
+    """
+    Evaluate multiple metrics for each column in the evaluation dataframe, grouped by source.
+
+    Parameters:
+    - eval_df (DataFrame): Evaluation dataframe.
+    - source_list (list): List of source names.
+    - column_list (list): List of column names to be evaluated.
+    - metric_list (list): List of metrics to be calculated ('bleu', 'sacrebleu', 'rouge', 'wer').
+
+    Returns:
+    - eval_dict_by_source (dict): Dictionary containing evaluation results for each source, column, and metric.
+    """
+    eval_dict_by_source = dict()
+    for src in source_list:
+        subset_eval_df = eval_df[eval_df['source'] == src]
+        subset_eval_dict = evaluate_all(subset_eval_df, column_list, metric_list)
+        eval_dict_by_source[src] = subset_eval_dict
+    return eval_dict_by_source
+
+
 def print_evaluation_results(eval_dict):
+    """
+    Print the evaluation results for each translation model and metric.
+
+    Parameters:
+    - eval_dict (dict): Dictionary containing evaluation results.
+
+    Output:
+    - Printed results in the console.
+    """
     for trans in eval_dict.keys():
         print(f"[{trans.upper()}]")
         for metric_key, metric_value in eval_dict[trans].items():
             print(f" - {metric_key.upper()}: {metric_value:.2f}")
+
+
+def save_eval_results_as_yaml(eval_dict, file_path):
+    """
+    Save the evaluation results to a YAML file.
+
+    Parameters:
+    - eval_dict (dict): Dictionary containing evaluation results.
+    - file_path (str): Path to the YAML file where results will be saved.
+    """
+    with open(file_path, 'w') as file:
+        yaml.dump(eval_dict, file)
 
 
 if __name__ == '__main__':
@@ -114,9 +251,28 @@ if __name__ == '__main__':
     deepl_trans: 딥엘 API
     opus_trans: Helsinki-NLP/opus-mt-tc-big-en-ko (HuggingFace)
     mbart_trans: facebook/mbart-large-50-many-to-many-mmt (HuggingFace)
-    nllb_trans: facebook/nllb-200-distilled-600M (HuggingFace)
+    nllb-600m_trans: facebook/nllb-200-distilled-600M (HuggingFace)
+    nllb-1.3b_trans: facebook/nllb-200-distilled-1.3B (HuggingFace)
+    madlad_trans: google/madlad400-3b-mt (HuggingFace)
+
+    [METRIC_LIST]
+    bleu: 
+     - BLEU (Bi-Lingual Evaluation Understudy)
+     - 번역에서 가장 흔하게 사용되나 띄어쓰기 기준 단어 단위로 평가해 한국어 평가에 다소 부적합한 면이 있음
+     - generation 단어가 reference에 얼마나 포함되는지
+    sacrebleu: 
+     - SacreBLEU
+     - BLEU와 원리가 같으나 띄어쓰기 단어 기준인 BLEU와 달리 토큰 단위로 평가해 한국어 평가에 조금 더 적합
+    rouge: 
+     - Rouge-1 / Rouge-2
+     - 번역보다는 요약 task에서 주로 사용하며, Rouge-L도 있으나 거의 Rouge-1과 동일하여 제외
+     - reference 단어가 generation에 얼마나 포함되는지
+    wer: 
+     - Word Error Rate
+     - 번역보다는 음성 인식 task에서 주로 사용
+     - 단순하게 generation과 reference 간 단어 단위 오류율로 계산
     """
-    column_list = ['nllb_trans']
-    metric_list = ['bleu', 'sacrebleu', 'rouge', 'wer']
+    column_list = ['nllb-1.3b_trans']
+    metric_list = ['bleu', 'sacrebleu', 'rouge', 'wer'] # bleu, sacrebleu, rouge, wer
     eval_dict = evaluate_all(eval_df, column_list, metric_list)
     print_evaluation_results(eval_dict)
