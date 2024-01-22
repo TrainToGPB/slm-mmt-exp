869dfff1f200c8093be87cd3f743f76005a74427
Sehyeong Kim <sehyeong_kim@tmax.co.kr>
2024-01-22 08:57:34 +0000

    docs: edit docs for update

--- inference/codes/model_inferences.py
+++ inference/codes/model_inferences.py
@@ -19,14 +19,14 @@ from custom_utils.training_utils import set_seed
 
 def load_model_and_tokenizer(model_type):
     """
-    Load the pre-trained translation model and tokenizer based on the specified model type.
+    Load a pre-trained language model and tokenizer based on the specified model type.
 
     Parameters:
-    - model_type (str): Type of the translation model ('opus', 'mbart', 'nllb-600m', 'nllb-1.3b', 'madlad').
+    - model_type (str): Type of the pre-trained language model.
 
     Returns:
-    - model (PreTrainedModel): Loaded translation model.
-    - tokenizer (PreTrainedTokenizer): Loaded tokenizer corresponding to the model.
+    - model (PreTrainedModel): Loaded pre-trained language model.
+    - tokenizer (PreTrainedTokenizer): Loaded tokenizer.
     """
     assert model_type in ['opus', 'mbart', 'nllb-600m', 'nllb-1.3b', 'madlad', 'mbart-aihub'], 'Wrong model type'
 
@@ -64,18 +64,18 @@ def load_model_and_tokenizer(model_type):
 
 def translate(model, tokenizer, text, model_type, device, max_length=512):
     """
-    Translate text from English to Korean using the specified translation model and tokenizer.
+    Translate a given text using the specified pre-trained language model.
 
     Parameters:
-    - model (PreTrainedModel): Loaded translation model.
-    - tokenizer (PreTrainedTokenizer): Loaded tokenizer corresponding to the model.
-    - text (str): English text to be translated.
-    - model_type (str): Type of the translation model ('opus', 'mbart', 'nllb-600m', 'nllb-1.3b', 'madlad').
-    - device (str): Device to use for inference ('cpu' or 'cuda').
-    - max_length (int, optional): Maximum length of the translated text. Default is 512.
+    - model (PreTrainedModel): Pre-trained language model.
+    - tokenizer (PreTrainedTokenizer): Tokenizer for encoding the input.
+    - text (str): Input text to be translated.
+    - model_type (str): Type of the pre-trained language model.
+    - device (str): Device for model inference (e.g., 'cuda' for GPU).
+    - max_length (int): Maximum length of the translated sequence.
 
     Returns:
-    - translated_text (str): Translated Korean text.
+    - str: Translated text.
     """
     model.to(device)
 
@@ -104,18 +104,15 @@ def translate(model, tokenizer, text, model_type, device, max_length=512):
 
 def inference(model_type, source_column, target_column, eval_path, output_path, device):
     """
-    Perform inference using the specified translation model on a DataFrame and save the results to a CSV file.
+    Perform inference on a dataset using the specified pre-trained language model.
 
     Parameters:
-    - model_type (str): Type of the translation model ('opus', 'mbart', 'nllb-600m', 'nllb-1.3b', 'madlad').
-    - source_column (str): Name of the column containing English text in the DataFrame.
-    - target_column (str): Name of the column to store the translated Korean text in the DataFrame.
-    - eval_path (str): Path to the CSV file containing the evaluation DataFrame.
-    - output_path (str): Path to save the output CSV file with translated text.
-    - device (str): Device to use for inference ('cpu' or 'cuda').
-
-    Returns:
-    - None: The results are saved to the specified output_path.
+    - model_type (str): Type of the pre-trained language model.
+    - source_column (str): Column name containing source texts in the evaluation dataset.
+    - target_column (str): Column name to store translated texts in the output dataset.
+    - eval_path (str): File path of the evaluation dataset in CSV format.
+    - output_path (str): File path to save the translated output in CSV format.
+    - device (str): Device for model inference (e.g., 'cuda' for GPU).
     """
     set_seed(42)
     model, tokenizer = load_model_and_tokenizer(model_type)
@@ -128,18 +125,15 @@ def inference(model_type, source_column, target_column, eval_path, output_path, 
 
 def inference_single(model_type, text, device):
     """
-    Perform inference using the specified translation model on a DataFrame and save the results to a CSV file.
+    Perform inference on a single text using the specified pre-trained language model.
 
     Parameters:
-    - model_type (str): Type of the translation model ('opus', 'mbart', 'nllb-600m', 'nllb-1.3b', 'madlad').
-    - source_column (str): Name of the column containing English text in the DataFrame.
-    - target_column (str): Name of the column to store the translated Korean text in the DataFrame.
-    - eval_path (str): Path to the CSV file containing the evaluation DataFrame.
-    - output_path (str): Path to save the output CSV file with translated text.
-    - device (str): Device to use for inference ('cpu' or 'cuda').
+    - model_type (str): Type of the pre-trained language model.
+    - text (str): Input text to be translated.
+    - device (str): Device for model inference (e.g., 'cuda' for GPU).
 
     Returns:
-    - None: The results are saved to the specified output_path.
+    - str: Translated text.
     """
     set_seed(42)
     model, tokenizer = load_model_and_tokenizer(model_type)
@@ -166,12 +160,12 @@ if __name__ == '__main__':
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print("DEVICE:", DEVICE)
 
-    model_types = ['mbart-aihub']
+    model_types = ['mbart', 'nllb-600m', 'madlad']
     for MODEL_TYPE in model_types:
         TARGET_COLUMN = MODEL_TYPE + "_trans"
-        # inference(MODEL_TYPE, SOURCE_COLUMN, TARGET_COLUMN, EVAL_PATH, OUTPUT_PATH, DEVICE)
+        inference(MODEL_TYPE, SOURCE_COLUMN, TARGET_COLUMN, EVAL_PATH, OUTPUT_PATH, DEVICE)
 
-        TEXT_KO = "엔믹스는 한국의 걸그룹으로, 2024년 1월 15일 신곡 'DASH'로 컴백했습니다."
-        TEXT_EN = "NMIXX is a South Korean girl group that made a comeback on January 15, 2024 with their new song 'DASH'."
-        translation = inference_single(MODEL_TYPE, TEXT_EN, DEVICE)
-        print(translation)
+        # TEXT_KO = "엔믹스는 한국의 걸그룹으로, 2024년 1월 15일 신곡 'DASH'로 컴백했습니다."
+        # TEXT_EN = "NMIXX is a South Korean girl group that made a comeback on January 15, 2024 with their new song 'DASH'."
+        # translation = inference_single(MODEL_TYPE, TEXT_EN, DEVICE)
+        # print(translation)
