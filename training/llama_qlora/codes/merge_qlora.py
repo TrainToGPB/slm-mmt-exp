a4e9354c1d1d3ff2a2a0c0bad7c8141c0c7de166
Sehyeong Kim <sehyeong_kim@tmax.co.kr>
2024-02-14 16:48:04 +0900

    feat: add merging and upscaling code
    - NF4 + BF16의 QLoRA를 BF16으로 업스케일링 후 병합
    - BF16 병합 버전을 FP16으로 변환

---
+++ training/llama_qlora/codes/merge_qlora.py
@@ -0,0 +1,150 @@
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '2'
+import gc
+import json
+import copy
+
+import torch
+import shutil
+from tqdm import tqdm
+from peft import PeftModel
+from peft.utils import _get_submodules
+import bitsandbytes as bnb
+from bitsandbytes.functional import dequantize_4bit
+from transformers import LlamaForCausalLM, LlamaTokenizer, BitsAndBytesConfig
+
+
+def save_model(model, tokenizer, to):
+    print(f"Saving dequantized model to {to}...")
+    model.save_pretrained(to)
+    tokenizer.save_pretrained(to)
+    config_data = json.loads(open(os.path.join(to, 'config.json'), 'r').read())
+    config_data.pop("quantization_config", None)
+    config_data.pop("pretraining_tp", None)
+    with open(os.path.join(to, 'config.json'), 'w') as config:
+        config.write(json.dumps(config_data, indent=2))
+    
+
+def dequantize_model(model, tokenizer, to=None, dtype=torch.bfloat16, device=torch.cuda.current_device()):
+    """
+    'model': the peftmodel you loaded with qlora.
+    'tokenizer': the model's corresponding hf's tokenizer.
+    'to': directory to save the dequantized model
+    'dtype': dtype that the model was trained using
+    'device': device to load the model to
+    """
+    cls = bnb.nn.Linear4bit
+
+    with torch.no_grad():
+        tqdm_iterator = tqdm(model.named_modules(), total=len(list(model.named_modules())))
+        tqdm_iterator.set_description('[DEQUANTIZING LAYERS]')
+        for name, module in tqdm_iterator:
+            if isinstance(module, cls):
+                quant_state = copy.deepcopy(module.weight.quant_state)
+                quant_state.dtype = dtype
+
+                weights = dequantize_4bit(module.weight.data, quant_state=quant_state, quant_type="nf4").to(dtype)
+
+                new_module = torch.nn.Linear(module.in_features, module.out_features, bias=None, dtype=dtype)
+                new_module.weight = torch.nn.Parameter(weights)
+                new_module.to(device=device, dtype=dtype)
+
+                parent, target, target_name = _get_submodules(model, name)
+                setattr(parent, target_name, new_module)
+
+                print(f"{name} is dequantized...")
+
+        model.is_loaded_in_4bit = False
+
+        if to is not None:
+            if os.path.exists(to):
+                shutil.rmtree(to)
+
+            os.makedirs(to, exist_ok=True)
+
+            save_model(model, tokenizer, to)
+        
+        return model
+        
+
+def dequantize_and_save(
+        model_path,
+        adapter_path,
+        save_dequant_plm_path,
+        save_dequant_merged_path
+    ):
+    quantization_config=BitsAndBytesConfig(
+        load_in_4bit=True,
+        bnb_4bit_compute_dtype=torch.bfloat16,
+        bnb_4bit_use_double_quant=True,
+        bnb_4bit_quant_type="nf4",
+    )
+
+    try:
+        print(f"Starting to load the model {model_path} into memory")
+
+        model = LlamaForCausalLM.from_pretrained(
+            model_path,
+            load_in_4bit=True,
+            torch_dtype=torch.bfloat16,
+            quantization_config=quantization_config,
+            device_map=torch.cuda.current_device()
+        )
+        print(model)
+        tokenizer = LlamaTokenizer.from_pretrained(model_path)
+        
+        model = dequantize_model(
+            model, 
+            tokenizer, 
+            to=save_dequant_plm_path
+        )
+        
+        print(model)
+        model = PeftModel.from_pretrained(model, adapter_path)
+        print(model)
+        model = model.merge_and_unload()
+        print(model)
+        
+        print(f"Successfully loaded the model {model_path} into memory")
+        
+        save_model(model, tokenizer, save_dequant_merged_path)
+        
+        print(f"Successfully saved merged model {model_path} to disk")
+
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+        if 'model' in locals():
+            del model
+
+        torch.cuda.empty_cache()
+
+        gc.collect()
+
+        print("Model, GPU cache, and garbage have been cleared.")
+
+
+def convert_bf16_to_fp16(model_path, save_path):
+    model = LlamaForCausalLM.from_pretrained(model_path, torch_dtype=torch.bfloat16)
+    tokenizer = LlamaTokenizer.from_pretrained(model_path)
+    for param in model.parameters():
+        if param.requires_grad:
+            param.data = param.data.half()
+
+    for name, param in model.named_parameters():
+        print(f'{name}: {param.dtype}')
+
+    save_model(model, tokenizer, save_path)
+
+
+if __name__ == '__main__':
+    model_path = 'beomi/open-llama-2-ko-7b'
+    adapter_path = '../models/baseline'
+    save_dequant_plm_path = '../models/open-llama-2-ko-7b-bf16-dequant-from-nf4'
+    save_dequant_merged_bf16_path = '../models/baseline-merged-bf16'
+    save_dequant_merged_fp16_path = '../models/baseline-merged-fp16'
+
+    # dequantize_and_save(model_path, adapter_path, save_dequant_plm_path, save_dequant_merged_bf16_path)
+
+    convert_bf16_to_fp16(save_dequant_merged_bf16_path, save_dequant_merged_fp16_path)
+    
