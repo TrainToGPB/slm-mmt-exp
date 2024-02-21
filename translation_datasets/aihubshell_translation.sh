21d179f5613c2d22ecb1603cde728e9cb9bea020
traintogpb <sehyeong_kim@tmax.co.kr>
2024-02-21 18:50:04 +0900

    data: add aihub mmt dataset download and handling codes

--- translation_datasets/aihubshell_translation.sh
+++ translation_datasets/aihubshell_translation.sh
@@ -1,19 +1,35 @@
-# 전문분야
-aihubshell -mode d -datasetkey 111
-# 기술과학
-aihubshell -mode d -datasetkey 124
-# 사회과학
-aihubshell -mode d -datasetkey 125
-# 병렬
-aihubshell -mode d -datasetkey 126
-# 산업정보(특허)
-aihubshell -mode d -datasetkey 563
-# 일상생활 및 구어체
-aihubshell -mode d -datasetkey 71265
-# 기술과학
-aihubshell -mode d -datasetkey 71266
-# 방송콘텐츠
-aihubshell -mode d -datasetkey 71382
+# # 한-영 번역 데이터셋
+# # 전문분야
+# aihubshell -mode d -datasetkey 111
+# # 기술과학
+# aihubshell -mode d -datasetkey 124
+# # 사회과학
+# aihubshell -mode d -datasetkey 125
+# # 병렬
+# aihubshell -mode d -datasetkey 126
+# # 산업정보(특허)
+# aihubshell -mode d -datasetkey 563
+# # 일상생활 및 구어체
+# aihubshell -mode d -datasetkey 71265
+# # 기술과학
+# aihubshell -mode d -datasetkey 71266
+# # 방송콘텐츠
+# aihubshell -mode d -datasetkey 71382
 
-# 기계번역 품질검증
-aihubshell -mode d -datasetkey 71475
+# # 기계번역 품질검증
+# aihubshell -mode d -datasetkey 71475
+
+
+# 다국어 번역 데이터셋
+# 일상생활구어체: 중/일
+aihubshell -mode d -datasetkey 546 -filekey 62133,62135
+# 관광지소개: 영/중/일
+aihubshell -mode d -datasetkey 71428 -filekey 498774,498775,498776,498777,498778,498779,498780,498781,498782,498783,498784,498785,498786,498787,498788,498789,498790,498791,498792,498793,498794,498795,498796,498797,498798,498799,498800,498801,498830,498831,498832,498833,498834,498835,498836,498837,498838,498839,498840,498841,498842,498843,498844,498845,498846,498847,498848,498849,498850,498851,498852,498853,498854,498855,498856,498857
+# 기술과학: 중/일
+aihubshell -mode d -datasetkey 71493 -filekey 465479,465480,465483,465484
+# 기초과학: 영/중/일
+aihubshell -mode d -datasetkey 71496 -filekey 465492,465493,465494,465498,465499,465500
+# 인문학: 영/중/일
+aihubshell -mode d -datasetkey 71498 -filekey 465504,465505,465506,465510,465511,465512
+# 낭독체: 영/일
+aihubshell -mode d -datasetkey 71524 -filekey 505232,505233,505234,505240,505241,505242,505243,505244,505245,505246,505247,505264,505265,505266,505272,505273,505274,505275,505276,505277,505278,505279
