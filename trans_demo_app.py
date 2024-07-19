2e0ae00eba0606addc4bb6916318e06f3a8ab414
traintogpb <kaiba0514@gmail.com>
2024-07-19 15:22:18 +0900

    refactor: modulize streamlit demo code

--- trans_demo_app.py
+++ trans_demo_app.py
@@ -10,12 +10,53 @@ from transformers import AutoTokenizer
 from vllm import LLM, SamplingParams
 from vllm.lora.request import LoRARequest
 
+
+MODEL_NAME = st.secrets["model"]["name"]
+VRAM_LIMIT = st.secrets["model"]["vramLimit"]
+MAX_LENGTH = st.secrets["model"]["maxLength"]
+USE_LORA = st.secrets["model"]["useLora"]
+MAX_LORA_RANK = st.secrets["model"]["maxLoraRank"]
 LANG_TABLE = {
     'en': 'English',
     'ko': '한국어',
     'ja': '日本語',
     'zh': '中文',
 }
+LORA_DICT = {
+    'mmt': {
+        'name': 'mmt-adapter', 
+        'id': 1, 'path': 
+        '/data/sehyeong/nmt/models/mmt_ft/ko-enjazh/ko/'
+    },
+    'ko-ja': {
+        'name': 'koja-adapter', 
+        'id': 3, 
+        'path': '/data/sehyeong/nmt/models/mmt_ft/ko-ja/koja-halfja/'
+    },
+    'ko-zh': {
+        'name': 'kozh-adapter', 
+        'id': 4, 
+        'path': '/data/sehyeong/nmt/models/mmt_ft/ko-zh/ko/'
+    },
+}
+
+
+def initialize_session_state():
+    defaults = {
+        'src_lang': 'ko',
+        'tgt_lang': 'en',
+        'adapter': 'mmt',
+        'temperature': 0.0,
+        'top_k': 40,
+        'top_p': 0.95,
+        'is_structured': False,
+        'stream_text': True,
+        'input_text': ""
+    }
+    for key, value in defaults.items():
+        if key not in st.session_state:
+            st.session_state[key] = value
+    return st.session_state
 
 @st.cache_resource
 def load_model(model_name, vram_limit=0.95, max_length=1024, use_lora=False, **kwargs):
@@ -33,6 +74,24 @@ def load_model(model_name, vram_limit=0.95, max_length=1024, use_lora=False, **k
     )
     return model
 
+def load_model_and_tokenizer(model_name):
+    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
+    model = load_model(model_name, vram_limit=VRAM_LIMIT, max_length=MAX_LENGTH, use_lora=USE_LORA, max_lora_rank=MAX_LORA_RANK)
+    return model, tokenizer
+
+def load_sampling_params(tokenizer):
+    sampling_params = SamplingParams(
+        temperature=st.session_state['temperature'],
+        use_beam_search=False,
+        top_k=st.session_state['top_k'],
+        top_p=st.session_state['top_p'],
+        skip_special_tokens=True,
+        stop=tokenizer.eos_token,
+        repetition_penalty=st.secrets["model"]["repetitionPenalty"],
+        max_tokens=st.secrets["model"]["maxTokensToGenerate"],
+    )
+    return sampling_params
+
 def preprocess_text(input_text, is_structured=False):
     input_text = input_text.strip()
 
@@ -49,7 +108,6 @@ def preprocess_text(input_text, is_structured=False):
         texts = [text[len(init_simbol):].strip() for text, init_simbol in zip(texts, init_symbols)]
     else:
         init_symbols = None
-
     return texts, line_breaks, init_symbols
 
 def make_prompt(text, src_lang, tgt_lang):
@@ -92,6 +150,35 @@ def translate(input_text, model, sampling_params, lora_request=None, src_lang='k
 
     return translation, translation_time
 
+def setup_translate(model, lora_dict, sampling_params, session_state):
+    if session_state['input_text'].strip() == "":
+        st.warning("Please enter some text to translate.")
+    else:
+        with st.spinner("Translating..."):
+            lora_request = LoRARequest(lora_dict[session_state['adapter']]['name'], 
+                                       lora_dict[session_state['adapter']]['id'], 
+                                       lora_dict[session_state['adapter']]['path'])
+            try:
+                translation, translation_time = translate(session_state['input_text'], 
+                                                          model, 
+                                                          sampling_params, 
+                                                          lora_request, 
+                                                          src_lang=session_state['src_lang'], 
+                                                          tgt_lang=session_state['tgt_lang'], 
+                                                          is_structured=session_state['is_structured'])
+                session_state['translation_for_print'] = '  \n'.join(translation.split('\n'))
+                if session_state['stream_text']:
+                    st.write_stream(stream_single(text=session_state['translation_for_print']))
+                else:
+                    st.text_area(label=f"Translated {LANG_TABLE[session_state['tgt_lang']]} text", 
+                                 value=session_state['translation_for_print'],
+                                 height=400)
+                st.success(f"Translation completed in {translation_time:.2f} ms")
+            except Exception as e:
+                st.error(f"An error occurred during translation: {e}")
+                
+    return session_state
+
 def stream_single(text):
     for paragraph in text.split('  \n'):
         for word in paragraph.split():
@@ -101,101 +188,53 @@ def stream_single(text):
             yield ' '
         yield '  \n'
 
-def main():
-    st.title("SPARTA-large Translator")
-
-    if 'src_lang' not in st.session_state:
-        st.session_state['src_lang'] = 'ko'
-    if 'tgt_lang' not in st.session_state:
-        st.session_state['tgt_lang'] = 'en'
-    if 'adapter' not in st.session_state:
-        st.session_state['adapter'] = 'mmt'
-    if 'temperature' not in st.session_state:
-        st.session_state['temperature'] = 0.0
-    if 'top_k' not in st.session_state:
-        st.session_state['top_k'] = 40
-    if 'top_p' not in st.session_state:
-        st.session_state['top_p'] = 0.95
-    if 'is_structured' not in st.session_state:
-        st.session_state['is_structured'] = False
-    if 'stream_text' not in st.session_state:
-        st.session_state['stream_text'] = True
-    if 'input_text' not in st.session_state:
-        st.session_state['input_text'] = ""
-
+def setup_sidebar(session_state):
     st.sidebar.title("Settings")
+
     st.sidebar.header("Language")
-    st.session_state['src_lang'] = st.sidebar.selectbox("Source Language", options=list(LANG_TABLE.keys()), format_func=lambda x: LANG_TABLE[x], index=1)
-    st.session_state['tgt_lang'] = st.sidebar.selectbox("Target Language", options=list(LANG_TABLE.keys()), format_func=lambda x: LANG_TABLE[x], index=0)
+    session_state['src_lang'] = st.sidebar.selectbox("Source Language", options=list(LANG_TABLE.keys()), format_func=lambda x: LANG_TABLE[x], index=1)
+    session_state['tgt_lang'] = st.sidebar.selectbox("Target Language", options=list(LANG_TABLE.keys()), format_func=lambda x: LANG_TABLE[x], index=0)
     st.sidebar.markdown("---")
+
     st.sidebar.header("Model Parameters")
-    st.session_state['adapter'] = st.sidebar.selectbox("Adapter", ["mmt", "ko-ja", "ko-zh"], placeholder="mmt")
-    st.session_state['temperature'] = st.sidebar.slider("Temperature", 0.0, 1.0, 0.0)
-    st.session_state['top_k'] = st.sidebar.slider("Top-k", 1, 100, 40)
-    st.session_state['top_p'] = st.sidebar.slider("Top-p", 0.1, 1.0, 0.95)
+    session_state['adapter'] = st.sidebar.selectbox("Adapter", ["mmt", "ko-ja", "ko-zh"], placeholder="mmt")
+    session_state['temperature'] = st.sidebar.slider("Temperature", 0.0, 1.0, 0.0)
+    session_state['top_k'] = st.sidebar.slider("Top-k", 1, 100, 40)
+    session_state['top_p'] = st.sidebar.slider("Top-p", 0.1, 1.0, 0.95)
     st.sidebar.markdown("---")
+
     st.sidebar.header("Others")
-    st.session_state['is_structured'] = st.sidebar.checkbox("Structured Text", False)
-    st.session_state['stream_text'] = st.sidebar.toggle("Stream Text", True)
+    session_state['is_structured'] = st.sidebar.checkbox("Structured Text", True)
+    session_state['stream_text'] = st.sidebar.toggle("Stream Text", False)
     st.sidebar.warning("'Stream Text' option does not support Markdown.")
 
-    model_name = 'beomi/Llama-3-Open-Ko-8B'
-    tokenizer = AutoTokenizer.from_pretrained(model_name, trust_remote_code=True)
-    with st.spinner(f"Trying to load translator model..."):
-        model = load_model(model_name, 
-                           vram_limit=0.95, 
-                           max_length=1024, 
-                           use_lora=True, 
-                           max_lora_rank=64)
-    
-    sampling_params = SamplingParams(
-        temperature=st.session_state['temperature'],
-        use_beam_search=False,
-        top_k=st.session_state['top_k'],
-        top_p=st.session_state['top_p'],
-        skip_special_tokens=True,
-        stop=tokenizer.eos_token,
-        repetition_penalty=1.1,
-        max_tokens=1024,
-    )
+    return session_state
+
+def input_area(session_state):
+    session_state['input_text'] = st.text_area("", "", height=400, placeholder="Type anything you want to translate...")
+    return session_state
+
+def setup_ui(session_state):
+    session_state = setup_sidebar(session_state)
+    st.header("Translate Text")
+    session_state = input_area(session_state)
+    return session_state
+
+def main():
+    st.title("SPARTA-large Translator")
+    st.session_state = initialize_session_state()
+    st.session_state = setup_ui(st.session_state)
+
+    model, tokenizer = load_model_and_tokenizer(MODEL_NAME)
+    sampling_params = load_sampling_params(tokenizer)
     lora_dict = {
         'mmt': {'name': 'mmt-adapter', 'id': 1, 'path': '/data/sehyeong/nmt/models/mmt_ft/ko-enjazh/ko/'},
-        # 'ko-en': {'name': 'koen-adapter', 'id': 2, 'path': '/data/sehyeong/nmt/models/mmt_ft/ko-en/ko/'},
         'ko-ja': {'name': 'koja-adapter', 'id': 3, 'path': '/data/sehyeong/nmt/models/mmt_ft/ko-ja/koja-halfja/'},
         'ko-zh': {'name': 'kozh-adapter', 'id': 4, 'path': '/data/sehyeong/nmt/models/mmt_ft/ko-zh/ko/'},
     }
 
-    st.header("Translate Text")
-    st.session_state['input_text'] = st.text_area("", "", height=400, placeholder="Type anything you want to translate...")
-    
     if st.button("Translate"):
-        if st.session_state['input_text'].strip() == "":
-            st.warning("Please enter some text to translate.")
-        else:
-            with st.spinner("Translating..."):
-                lora_request = LoRARequest(lora_dict[st.session_state['adapter']]['name'], lora_dict[st.session_state['adapter']]['id'], lora_dict[st.session_state['adapter']]['path'])
-                try:
-                    translation, translation_time = translate(st.session_state['input_text'], 
-                                                              model, 
-                                                              sampling_params, 
-                                                              lora_request, 
-                                                              src_lang=st.session_state['src_lang'], 
-                                                              tgt_lang=st.session_state['tgt_lang'], 
-                                                              is_structured=st.session_state['is_structured'])
-                except Exception as e:
-                    st.error(f"An error occurred during translation: {e}")
-                    return
-                
-                translation_for_print = '  \n'.join(translation.split('\n'))
-
-                st.success(f"Translation completed in {translation_time:.2f} ms")
-                if st.session_state['stream_text']:
-                    st.write_stream(stream_single(text=translation_for_print))
-                else:
-                    st.write(translation_for_print)
-
-            print(st.session_state['input_text'])
-            print(translation_for_print)
+        st.session_state = setup_translate(model, lora_dict, sampling_params, st.session_state)
 
 if __name__ == "__main__":
     main()
